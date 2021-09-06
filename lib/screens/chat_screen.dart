fc30af5b68cda550888bde8b30f03de8f1bf9f91
Miikhaaeel <fadowomichael1@gmail.com>
2021-09-06 04:31:40 +0100

    firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -31,6 +31,21 @@ class _ChatScreenState extends State<ChatScreen> {
     } catch (e) {}
   }
 
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
